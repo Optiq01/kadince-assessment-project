fdab73283a2d688758c59ab2f8aba65126d649a7
Optiq01 <Optiqvision@gmail.com>
2022-12-16 01:22:06 -0600

    created a service file to expose todo data to app component

---
+++ src/app/app.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ToDoItemInterface } from '@site-types';
+import {BehaviorSubject, Observable} from 'rxjs';
+import { TODO_DATA } from 'src/assets/dummyData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  private TodoData     : BehaviorSubject<ToDoItemInterface[]> = new BehaviorSubject([...TODO_DATA]);
+  private ToDoObserver : Observable<ToDoItemInterface[]>      = this.TodoData.asObservable();
+
+  public getTodos() : Observable<ToDoItemInterface[]> { return this.ToDoObserver; }
+
+  constructor() { }
+}
