8c3f2119e38adf00562085f82cc23c5ec6cee3b3
Optiq01 <Optiqvision@gmail.com>
2022-12-16 01:30:01 -0600

    added app service to app component to iterate data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToDoItemInterface } from '@site-types';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kadince-assessment-app';
+export class AppComponent implements OnInit{
+
+  $TodoListObservable: ToDoItemInterface[] = [];
+  
+  constructor(private dataService: AppService){}
+
+  ngOnInit(): void {
+    this.dataService.getTodos().subscribe(a=>{ this.$TodoListObservable = a; });
+  }
 }
